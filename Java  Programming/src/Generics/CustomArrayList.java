5258e10d89c78e650f960dcd7f4c92e76f9c6938
Anshu Kumar <anshukr95086@gmail.com>
2025-08-05 20:51:48 +0530

    created my own ArrayList

--- Java  Programming/src/Generics/CustomArrayList.java
+++ Java  Programming/src/Generics/CustomArrayList.java
@@ -1,5 +1,58 @@
 package Generics;
 
-public class CustomArrayList {
+import java.util.Arrays;
 
+public class CustomArrayList {
+    private int[] data;
+    private static int DEFAULT_SIZE = 10;
+    private int size = 0;
+
+    public CustomArrayList(){
+        this.data = new int[DEFAULT_SIZE];
+    }
+
+    public void add(int num){
+        if(isFull()){
+            resize();
+        }
+       data[size++] = num;
+    }
+
+    public void remove(){
+        size--;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public int get(int index){
+        return data[index];
+    }
+
+    private boolean isFull(){
+        return size == data.length;
+    }
+
+    private void resize(){
+        int[] temp = new int[data.length * 2];
+        for (int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+            data = temp;
+        }
+    }
+
+    public String toString(){
+        return Arrays.toString(data);
+    }
+
+    public static void main(String[] args) {
+        CustomArrayList list = new CustomArrayList();
+        list.add(3);
+        list.add(9);
+        list.add(10);
+
+        System.out.println(list);
+        System.out.println(list.size());
+    }
 }
